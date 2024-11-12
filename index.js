996ac09842cc18c35576fae1961606917f7b2268
NODEBOY <kingindoslayer@gmail.com>
2024-11-12 19:00:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const randomDelay = (min, max) => {
 
 const displayWelcome = () => {
     console.log(`
-\x1b[32m╔══════════════════════════════════════════════════════╗\x1b[0m
-\x1b[32m║\x1b[0m                \x1b[32mDAWN VALIDATOR BOT\x1b[0m                       \x1b[32m║\x1b[0m
-\x1b[32m║\x1b[0m              \x1b[36mAuthor : Nofan Rambe\x1b[0m                     \x1b[32m║\x1b[0m
-\x1b[32m║\x1b[0m            \x1b[36mWelcome & Enjoy Sir!\x1b[0m                      \x1b[32m║\x1b[0m
-\x1b[32m╚══════════════════════════════════════════════════════╝\x1b[0m
+\x1b[31m╔══════════════════════════════════════════════════════╗\x1b[0m
+\x1b[33m║\x1b[0m             \x1b[32mDAWN VALIDATOR BOT\x1b[0m                       \x1b[33m║\x1b[0m
+\x1b[33m║\x1b[0m             \x1b[36mAuthor : Nofan Rambe\x1b[0m                     \x1b[33m║\x1b[0m
+\x1b[33m║\x1b[0m            \x1b[36mWelcome & Enjoy Sir!\x1b[0m                      \x1b[33m║\x1b[0m
+\x1b[31m╚══════════════════════════════════════════════════════╝\x1b[0m
     `);
 };
 
